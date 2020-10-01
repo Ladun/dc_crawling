542f30fc02ba8fcdaba04b4355f8027f6fb104d5
이강준 <ladun9128@gmail.com>
2020-10-01 16:01:34 +0900

    저장 기능 추가

--- dc_crawler.py
+++ dc_crawler.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
-from config import headers, url
+from config import headers, url, json_path
 import time
+import json
+import os
 
 
 class DCinsideCrawler:
@@ -34,7 +36,6 @@ class DCinsideCrawler:
 
         # Build Webdriver
 
-
         self.driver.get(f"{url}/?id=ssu&no={no}")
 
         # Parse page_source to BeautifulSoup
@@ -45,23 +46,23 @@ class DCinsideCrawler:
             # 본문
             title = bs.find('span', class_="title_subject")
             title_text = title.text
-            view_box = bs.find('div', class_="writing_view_box").find('div', attrs={'style': "overflow:hidden;width:900px"})
+            view_box = bs.find('div', class_="writing_view_box").find('div',
+                                                                      attrs={'style': "overflow:hidden;width:900px"})
             content_text = view_box.text
             gall_date = bs.find('span', class_='gall_date').text
-
 
             # 댓글
             cmt_list = bs.find('ul', class_="cmt_list")
 
-            nicknames = cmt_list.find_all('span', class_="gall_writer ub-writer")
-            comment_contents = cmt_list.find_all('p', class_="usertxt ub-word")
-
             cmts = []
-            for nickname, content in zip(nicknames, comment_contents):
-                cmts.append({
-                    "nickname": nickname.text,
-                    "content": content.text
-                })
+            if cmt_list != None:
+                nicknames = cmt_list.find_all('span', class_="gall_writer ub-writer")
+                comment_contents = cmt_list.find_all('p', class_="usertxt ub-word")
+                for nickname, content in zip(nicknames, comment_contents):
+                    cmts.append({
+                        "nickname": nickname.text,
+                        "content": content.text
+                    })
 
             info = {
                 "title": title_text,
@@ -76,12 +77,33 @@ class DCinsideCrawler:
             return None
 
     def get_data(self, min_gall_no=1, max_gall_no=10):
-        data = {}
-        for no in range(min_gall_no, max_gall_no + 1):
+
+        json_data = {}
+        for no in range(min_gall_no, max_gall_no):
+            print(f"Parse {no} gallery")
             info = self.__parse_content(no)
 
             if info is None:
                 continue
 
-            data[no] = info
-        return data
+            json_data[no] = info
+        return json_data
+
+    def save_data(self, min_no=1, max_no=10, save_step=100):
+        if os.path.exists(json_path):
+            with open(json_path, "r", encoding="UTF-8") as f:
+                json_data = json.load(f)
+        else:
+            json_data = {}
+
+        st = min_no
+
+        with open(json_path, "w", encoding="UTF-8") as f:
+            while st < max_no:
+                ed = min(st + save_step, max_no)
+                info = self.get_data(st, ed)
+
+                json_data.update(info)
+                json.dump(json_data, f, ensure_ascii=False, indent="\t")
+                st = ed
+
--- test.py
+++ test.py
@@ -3,10 +3,5 @@ import json
 
 # crawler.get_data(1, 1)
 crawler = DCinsideCrawler()
-info = crawler.get_data(330000, 330000)
+crawler.save_data(1, 100000)
 
-print(json.dumps(info, ensure_ascii=False, indent=4))
-
-with open("./dc_crawling.json", "w", encoding="UTF-8") as f:
-    json.dump(info, f, ensure_ascii=False, indent="\t")
-    
